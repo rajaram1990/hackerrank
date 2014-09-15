366d535935ddb4b380982f006aeca8572b0965f5
= <rajaram.1990@gmail.com>
2014-09-15 20:07:20 +0530

    New problems

---
+++ warmup/service_lane.py
@@ -0,0 +1,17 @@
+"""
+    See Problem Statement in service-lane.pdf
+"""
+way_length, ntests = [int(var) for var in raw_input().split(' ')]
+way_width = [int(var) for var in raw_input().split(' ')]
+test_cases = [] # List of lists. Each sub list contains two elements i and j
+# Clavin Enters by i and exits by j
+for i in range(0,ntests):
+    test_cases.append([int(var) for var in raw_input().split(' ')])
+## The processsing
+print 'WIDTH %s'%(way_width)
+for case in test_cases:
+    i,j = case
+    print 'Processing Array %s'%(way_width[i:j])
+    if i==0:
+        print min(way_width
+    print min(way_width[i:j])
