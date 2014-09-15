13cb71236ab22399edf1e9c8937c6336249dbb16
Rajaram Srinivasan <rajaram.1990@gmail.com>
2014-09-16 01:35:04 +0530

    Solved service lane problem

--- warmup/service_lane.py
+++ warmup/service_lane.py
@@ -8,10 +8,8 @@ test_cases = [] # List of lists. Each sub list contains two elements i and j
 for i in range(0,ntests):
     test_cases.append([int(var) for var in raw_input().split(' ')])
 ## The processsing
-print 'WIDTH %s'%(way_width)
+#print 'WIDTH %s'%(way_width)
 for case in test_cases:
     i,j = case
-    print 'Processing Array %s'%(way_width[i:j])
-    if i==0:
-        print min(way_width
-    print min(way_width[i:j])
+#   print 'Processing Array %s'%(way_width[i:j])
+    print min(way_width[i:j+1])
