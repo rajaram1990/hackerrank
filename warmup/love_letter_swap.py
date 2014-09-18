3ab3b1adc99168c55ccd0e9ad31c3716158e2f9e
= <rajaram.1990@gmail.com>
2014-09-18 20:21:01 +0530

    Dump

--- warmup/love_letter_swap.py
+++ warmup/love_letter_swap.py
@@ -7,5 +7,15 @@ for string in orig_strs:
     left_index = 0
     right_index = len(string)-1
     while (right_index-left_index)>1:
-        left_chr = ord(left_index)
-        right_char = ord(right_index)
+        left_char = ord(string[left_index])
+        right_char = ord(string[right_index])
+        if left_char > right_char:
+            nops = left_char - right_char
+            string[left_index] = chr(ord(left_char)+(left_char - right_char))
+        elif right_char > left_char:
+            nops = right_char - left_char
+            string[right_index] = chr(ord(right_char)+(right_char - left_char))
+
+
+
+            
