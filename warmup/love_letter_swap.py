c1dff2ba093d71a81f95fa0adf99705567feeff0
= <rajaram.1990@gmail.com>
2014-09-24 19:58:57 +0530

    Dump

--- warmup/love_letter_swap.py
+++ warmup/love_letter_swap.py
@@ -6,16 +6,16 @@ for string in orig_strs:
     nops = 0
     left_index = 0
     right_index = len(string)-1
+    letters = []
+    for letter in string:
+        letters.append(letter)
     while (right_index-left_index)>1:
-        left_char = ord(string[left_index])
-        right_char = ord(string[right_index])
+        left_char = ord(letters[left_index])
+        right_char = ord(letters[right_index])
         if left_char > right_char:
             nops = left_char - right_char
-            string[left_index] = chr(ord(left_char)+(left_char - right_char))
+            letters[left_index] = chr(left_char+(left_char - right_char))
         elif right_char > left_char:
             nops = right_char - left_char
-            string[right_index] = chr(ord(right_char)+(right_char - left_char))
-
-
-
-            
+            letters[right_index] = chr(right_char+(right_char - left_char))
+    print nops            
