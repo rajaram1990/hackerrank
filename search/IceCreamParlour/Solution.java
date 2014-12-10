1a1d44ec4aaa8ead721b94a3b9995c588f1d6d84
Rajaram Srinivasan <rajaram.1990@gmail.com>
2014-12-10 23:05:18 +0530

    Few more hackerrank problems on searching

---
+++ search/IceCreamParlour/Solution.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Solution {
+    public int[] 
+    public static void main(String[] args) {
+        /* N -> No. of flavours available (int)
+         * M -> Amount in $
+         * T -> No. of test cases
+         * C -> Cost of flavours (arraylist) */
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int M,N;
+        int[] combinations = new int[T];
+        for(int i=0; i<T; i++)
+        {
+            M = sc.nextInt();
+            N = sc.nextInt();
+            for()
+        }
+        
+    }
+}
