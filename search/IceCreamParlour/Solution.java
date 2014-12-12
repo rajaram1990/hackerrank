7271a99a95efaef6ce08e0dbd614931e7bde0f14
Rajaram Srinivasan <rajaram.1990@gmail.com>
2014-12-12 20:55:36 +0530

    Adding IceCreamParlour solution! Finally!

--- search/IceCreamParlour/Solution.java
+++ search/IceCreamParlour/Solution.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 class TestCase {
     /** The class to store the result of the ice cream problem. 
       * Has indexes of the two flavours that can be bought with M Dollars
@@ -8,8 +7,12 @@ class TestCase {
     public int N; //No. of flavours available
     public int outputIndex1;
     public int outputIndex2;
-    public int[] C = new int[N];//Cost of each flavour
-    public TestCase(int M, int N, int[] C)
+    public ArrayList<Integer> C = new ArrayList<Integer>(N);//Cost of each flavour
+    public TestCase(int M, int N, ArrayList<Integer> C)
+    {
+        this.init(M,N,C);
+    }
+    public void init(int M, int N, ArrayList<Integer> C)
     {
         this.M = M;
         this.N = N;
@@ -21,20 +24,18 @@ class TestCase {
     */
     {
         int temp_cost = 0;
+        Object[] C = this.C.toArray();
         for (int i=0; i<C.length; i++)
         {
-            temp_cost = M - C[i];
-            System.out.format("M %d \n",M);
-            System.out.format("C[i] %d \n",C[i]);
-            System.out.format("temp_cost %d \n",temp_cost);
+            temp_cost = M - (Integer)C[i];
             for(int j=0; j<C.length; j++)
             {
                 if (i!=j)
                 {
-                    if (C[j]==temp_cost)
+                    if ((Integer)C[j]==temp_cost)
                     {
-                        this.outputIndex1 = i;
-                        this.outputIndex2 = j;
+                        this.outputIndex1 = i+1; // To compensate for zero indexing
+                        this.outputIndex2 = j+1;
                         return;
                     }
                 }
@@ -50,23 +51,23 @@ public class Solution {
          * T -> No. of test cases
          * C -> Cost of flavours (arraylist) */
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int M,N;
+        int T = Integer.parseInt(sc.nextLine());
+        ArrayList<Integer> costs = new ArrayList<Integer>();
         TestCase[] tc = new TestCase[T];
-        for(int i=0; i<T; i++)
+        for (int i=0; i<T; i++)
         {
-            M = sc.nextInt();
-            N = sc.nextInt();
-            int[] C = new int[N];
-            for(int j=0; j<N; j++)
-                C[i] = sc.nextInt();
-            System.out.println("Hello");
-            System.out.println(C);
-            tc[i] = new TestCase(M,N,C);
+            int M = Integer.parseInt(sc.nextLine());
+            int N = Integer.parseInt(sc.nextLine());
+            String cost_flavours = sc.nextLine();
+            String[] cost_splits = cost_flavours.split(" ");
+            costs.clear();
+            for (String c : cost_splits)
+                costs.add(Integer.parseInt(c));
+            tc[i] = new TestCase(M,N,costs);
+            tc[i].get_flavours();
         }
         for (int i=0; i<T; i++)
         {
-            tc[i].get_flavours();
             System.out.format("%d %d\n",tc[i].outputIndex1, tc[i].outputIndex2);
         }
         
