172f459e1cd95da5455a0aef8cc87af2b9a3aec1
Rajaram Srinivasan <rajaram.1990@gmail.com>
2014-12-16 10:01:45 +0530

    Next problem in search

---
+++ search/ClosestNumbers/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*
+
+public class Solution
+{
+    private int a;
+    Solution()
+    {
+        a=0;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
