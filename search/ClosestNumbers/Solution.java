82237cb312318b988596d23a7f249a426d309e41
Rajaram Srinivasan <rajaram.1990@gmail.com>
2014-12-18 09:31:25 +0530

    Getting Closest Integers Done

--- search/ClosestNumbers/Solution.java
+++ search/ClosestNumbers/Solution.java
@@ -1,14 +1,38 @@
-import java.util.*
-
+import java.util.*;
 public class Solution
 {
-    private int a;
-    Solution()
-    {
-        a=0;
-    }
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        int N;
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        N = Integer.parseInt(sc.nextLine());
+        String str = sc.nextLine();
+        String[] splits = str.split(" ");
+        for (String split: splits)
+        {
+            al.add(Integer.parseInt(split));
+        }
+        Collections.sort(al);
+        int min_diff = Integer.MAX_VALUE;
+        ArrayList<Integer>index1 = new ArrayList<Integer>();
+        int temp_diff;
+        Object[] al_array = al.toArray();
+        for (int i=0;i<N-1;i++) {
+            temp_diff = Math.abs((Integer)al_array[i]- (Integer)al_array[i+1]);
+            if (temp_diff < min_diff)
+                min_diff = temp_diff;
+        }
+        for(int i=0;i<N-1;i++) {
+            temp_diff = Math.abs((Integer)al_array[i]- (Integer)al_array[i+1]);
+            if (temp_diff==min_diff)
+                index1.add(i);
+        }
+        Iterator<Integer> alitr = index1.iterator();
+        while (alitr.hasNext()) {
+            int temp_idx = alitr.next();
+            System.out.format("%d %d ",(Integer)al_array[temp_idx],(Integer)al_array[temp_idx+1]);
+        }
+        System.out.format("\n");
     }
 }
