e9a51a3f3998ddaf03f7a198b99b095810c368a6
Rajaram Srinivasan <rajaram.1990@gmail.com>
2014-12-24 18:54:18 +0530

    NCR Table

--- combinatorics/ncr_table/Solution.java
+++ combinatorics/ncr_table/Solution.java
@@ -1,22 +1,7 @@
 import java.util.*;
+import java.math.*;
 public class Solution
 {
-    public static long fast_pow(long base, long n,long M) 
-    {
-        if(n==0)
-            return 1;
-        if(n==1)
-            return base;
-        long halfn = fast_pow(base,n/2,M);
-        if(n%2==0)
-            return ( halfn * halfn ) % M;
-        else
-            return ( ( ( halfn * halfn ) % M ) * base ) % M;
-    }
-    public static long findMMI_fermat(long n,long M)
-    {
-        return fast_pow(n,M-2,M);
-    }
     public static void main(String[] args)
     {
         HashMap<Integer,Long> facts = new HashMap<Integer,Long>();
@@ -33,7 +18,11 @@ public class Solution
             facts.put(1,n[i]);
             for(int j=2;j<n[i]/2+1;j++)
             {
-                facts.put(j,facts.get(j-1)*(n[i]-j+1)/j);
+                BigInteger mod, temp_j, temp;
+                mod = new BigInteger("1000000000");
+                temp_j = new BigInteger(Integer.toString(j));
+                temp = temp_j.modInverse(mod);
+                facts.put(j,facts.get(j-1)*(n[i]-j+1)*temp.longValue());
             }
             for(int j=0;j<=n[i];j++)
             {
