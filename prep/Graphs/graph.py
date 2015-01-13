781f87dfb6873f78e1078a0d777a9c4c515bcfe0
Rajaram Srinivasan <rajaram.1990@gmail.com>
2015-01-13 10:24:43 +0530

    Graph class python

--- prep/Graphs/graph.py
+++ prep/Graphs/graph.py
@@ -1,6 +1,22 @@
 class Graph:
     nodes = None
-    adj_matrix = None
-    adj_list = None
+    adj_matrix = [[]]
+    adj_list = {}
     representation_method = None
-    def __init__(representation_method=)
+    def __init__(representation_method=adj_list):
+        self.representation_method = representation_method
+    def build_graph():
+        print "Building Graph"
+        for node in self.nodes:
+            print "Enter all nodes that Node %d is connected to"%(node)
+            connection_string = raw_input()
+            connections = connection_string.split(" ")
+            adj_list[node] = int()
+    def build_minimum_spanning_tree():
+        # Using Kruskal's algorithm
+        built_nodes = []
+        unbuilt_nodes = self.nodes
+        for node in unbuilt_nodes:
+
+
+
