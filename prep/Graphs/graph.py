20ccc9adcfce2ecdb4684ffad41bc11d1d5787bb
Rajaram Srinivasan <rajaram.1990@gmail.com>
2015-01-24 18:05:10 +0530

    Adding Graph and associated functionx

--- prep/Graphs/graph.py
+++ prep/Graphs/graph.py
@@ -1,22 +1,53 @@
+import Queue
 class Graph:
-    nodes = None
+    no_of_nodes = 0
+    nodes = []
     adj_matrix = [[]]
     adj_list = {}
     representation_method = None
-    def __init__(representation_method=adj_list):
-        self.representation_method = representation_method
-    def build_graph():
+    
+    def build_graph(self,no_of_nodes):
+        """
+            Warning : To be used the first time only.
+                      Erases all nodes, if the graph contains any already!
+        """
         print "Building Graph"
-        for node in self.nodes:
+        self.no_of_nodes = no_of_nodes
+        for node in xrange(0,no_of_nodes):
+            print "Enter data represented by node %d"%(node)
             print "Enter all nodes that Node %d is connected to"%(node)
+            self.nodes.append(node)
             connection_string = raw_input()
             connections = connection_string.split(" ")
-            adj_list[node] = int()
-    def build_minimum_spanning_tree():
-        # Using Kruskal's algorithm
-        built_nodes = []
-        unbuilt_nodes = self.nodes
-        for node in unbuilt_nodes:
+            self.adj_list[node] = [int(conn) for conn in connections]
 
+    def find_via_dfs(self, start_node, visited=[]):
+        if start_node not in visited:
+            visited.append(start_node)
+            for node in self.adj_list[start_node]:
+                self.find_via_dfs(start_node = node, visited=visited)
+        print visited
 
+    def bfs(self):
+        q = Queue.Queue()
+        start_node = self.nodes[0]
+        q.put(start_node)
+        return self.find_via_bfs(q)
+
+    def find_via_bfs(self,q):
+        visited = []
+        while not q.empty():
+            node = q.get()
+            visited.append(node)
+            for temp_node in self.adj_list[node]:
+                if temp_node not in visited:
+                    print visited
+                    print temp_node
+                    q.put(temp_node)
+
+        
+graph = Graph()
+graph.build_graph(5)
+#graph.find_via_dfs(graph.nodes[0])
+graph.bfs()
 
